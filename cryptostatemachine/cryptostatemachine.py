13bcb1ea7ce0f221bba3cf8acf71dd51c5f69bfb
rastapavel <rastapavel76@gmail.com>
2016-08-06 19:58:48 +0200

    Got whole digest to work by concatenating time stamp and graph digests

--- cryptostatemachine/cryptostatemachine.py
+++ cryptostatemachine/cryptostatemachine.py
@@ -53,7 +53,7 @@ class StateMachine:
 
     def __init__(self):
         # Time stamping
-        self.time_stamp = self.hash_method(str(datetime.now())) #CHECK ON TIME ZONES!!!
+        self.time_stamp = self.hash_method(str(datetime.now())).digest() #CHECK ON TIME ZONES!!!
 
         # Define initial graph structure based on state machine / work flow
         self.graph = self.init_graph()
@@ -113,22 +113,27 @@ class StateMachine:
         self.g_digest = g_hash.digest()
         return g_hash.digest()
 
-    def state_digest(self):
-        """
-        Returns digest of hashed
-        :return:
-        """
-        g_hash = self.g_hash
-        return(g_hash.digest())
+    # def state_digest(self):
+    #     """
+    #     Returns digest of hashed
+    #     :return:
+    #     """
+    #     g_hash = self.g_hash
+    #
+    #     return(g_hash.digest())
 
-    def whole_hash(self):
+    def set_whole_hash(self):
         """Hash graph state and time stamp for unique instance identifier"""
-        whole_hash = self.hash_method(self.time_stamp)
-        #whole_hash = whole_hash.update(self.g_digest)
+        #whole_hash = self.hash_method(self.time_stamp)
+        # whole_hash = whole_hash.update(self.g_digest)
+        whole_hash = self.hash_method(self.time_stamp + self.g_digest) # TODO: Is this a good idea?
+        #whole_hash_digest = whole_hash.digest()
         self.whole_hash = whole_hash
+        self.whole_digest = whole_hash.digest()
+        #return(whole_hash_digest)
 
-    def whole_digest(self):
-        return(self.whole_hash.digest())
+    # def whole_digest(self):
+    #     return(self.whole_hash.digest())
 
     def update_node(self, node_name, sent_from, sent_to):
         """
